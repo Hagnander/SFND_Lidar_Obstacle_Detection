62ccc6f03db7912f78b3e795583f769a97366b44
Johan Hägnander <johan.hagnander@movinity.ai>
2021-03-21 08:27:01 +0100

    feat: Separated the pointcloud into a point cloud for the plane (road) and for the obstacles

--- src/environment.cpp
+++ src/environment.cpp
@@ -51,10 +51,13 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
 
     //renderRays(viewer, lidar->position, inputCloud);
-    renderPointCloud(viewer, inputCloud, "Hej");
-    //void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr& viewer, const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, std::string name, Color color = Color(1,1,1));
-
+    renderPointCloud(viewer, inputCloud, "inputCloud");
+    
     // TODO:: Create point processor
+    ProcessPointClouds<pcl::PointXYZ> pointProcessor;
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
+    renderPointCloud(viewer, segmentCloud.second, "planetCloud", Color(0,1,0));
   
 }
 
