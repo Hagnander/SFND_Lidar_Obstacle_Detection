cd86a4ca080141982f5dfe360589bddee4efadb3
Johan Hägnander <johan.hagnander@movinity.ai>
2021-03-29 21:37:10 +0200

    feat: extended the Ransac function to work with 3D data

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -67,6 +67,7 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	srand(time(NULL));
 	pcl::PointCloud<pcl::PointXYZ>::Ptr p1;
 	pcl::PointCloud<pcl::PointXYZ>::Ptr p2;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr p3;
 	
 	// TODO: Fill in this function
 
@@ -74,25 +75,32 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	while(maxIterations--)
 	{
 		std::unordered_set<int> inliers;
-		//Use 2 points to create a line
-		while(inliers.size() < 2 )
+		//Use 3 points to create a plane
+		while(inliers.size() < 3 )
 			inliers.insert(rand() %(cloud->points.size()));
 
-		float x1, x2, y1, y2;
+		float x1, x2, x3, y1, y2, y3, z1, z2, z3;
 		auto itr = inliers.begin();
 		x1 = cloud->points[*itr].x;
 		y1 = cloud->points[*itr].y;
+		z1 = cloud->points[*itr].z;
 		itr++;
 		x2 = cloud->points[*itr].x;
 		y2 = cloud->points[*itr].y;
+		z2 = cloud->points[*itr].z;
+		itr++;
+		x3 = cloud->points[*itr].x;
+		y3 = cloud->points[*itr].y;
+		z3 = cloud->points[*itr].z;
+
+		float a = ((y2 - y1) * (z3 - z1)) - ((z2 - z1) * (y3 - y1));
+        float b = ((z2 - z1) * (x3 - x1)) - ((x2 - x1) * (z3 - z1));
+        float c = ((x2 - x1) * (y3 - y1)) - ((y2 - y1) * (x3 - x1));
+        float d = -1 * (a * x1 + b * y1 + c * z1);
 
 
 
-		//p1 = cloud->points[(cloud->size() + 1) * rand()];
-		//p2 = cloud->points[(cloud->size() + 1) * rand()];
-		float A = y1 - y2;
-		float B = x2 - x1;
-		float C = x1 * y2 - x2 * y1;
+		
 
 		for(int index = 0; index < cloud->points.size(); index++)
 		{
@@ -100,10 +108,11 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 				continue;
 
 			pcl::PointXYZ point = cloud->points[index];
-			float x3 = point.x;
-			float y3 = point.y;
+			float x4 = point.x;
+			float y4 = point.y;
+			float z4 = point.z;
 
-			float d = fabs(A * x3 + B * y3 + C)/sqrt(A * A + B * B);
+			float d = fabs(a * x4 + b * y4 + c * z4 + d)/sqrt(a * a + b * b + c * c);
 			
 			if (d <= distanceTol)
 				inliers.insert(index);
